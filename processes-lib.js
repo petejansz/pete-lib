d30bafa4fda8e59eaa38ae1413494e261d239b53
Pete Jansz <peterjansz@gmail.com>
2018-08-16 10:14:10 -0700

    Moved add note function to processes-account.js, which shares use of axios

--- processes-lib.js
+++ processes-lib.js
@@ -36,63 +36,11 @@ var Processes = ( function ()
             }
 
         return request
-    },
-
-    createNote = function ( host, playerId, responseHandler, errorHandler, noteText )
-    {
-        var text = noteText ? noteText : 'Make a note @ ' + new Date()
-        const note =
-            {
-                alert: false,
-                id: null,
-                playerId: playerId,
-                status: 1,
-                type: 1,
-                value: text,
-                priority: 1,
-                user: 'administrator',
-            }
-
-        const body =
-            {
-                callingClientId: lib1.getFirstIPv4Address(),
-                transactionIdBase: lib1.generateUUID(),
-                transactionTime: new Date().valueOf(),
-                callerChannelId: lib1.caConstants.channelId,
-                callerSystemId: lib1.caConstants.systemId,
-                siteID: lib1.caConstants.siteID,
-                playerId: playerId,
-                note: note
-            }
-
-        const restPath = '/california/api/v1/processes/player-note'
-        const url = util.format( '%s://%s:%s%s', 'http', host, lib1.crmProcessesPort, restPath )
-        var options =
-            {
-                method: 'POST',
-                uri: url,
-                headers: lib1.commonHeaders,
-                body: body,
-                json: true
-            }
-
-        options.headers['x-player-id'] = playerId
-
-        request( options )
-            .then( function ( resp )
-            {
-                responseHandler( resp )
-            } )
-            .catch( function ( err )
-            {
-                errorHandler( err )
-            } )
     }
 
     return {
         createAxiosInstance: createAxiosInstance,
-        createProcessesRequest: createProcessesRequest,
-        createNote: createNote
+        createProcessesRequest: createProcessesRequest
     }
 } )()
 
