d3fa8ba5806f0791f9a3945ae403ef6af0fe76f9
Pete Jansz <peterjansz@gmail.com>
2018-11-09 09:35:45 -0800

    Fix createHeaders for mobile, add creds to createLoginRequest

--- igt-cas.js
+++ igt-cas.js
@@ -42,7 +42,7 @@ var IGTCas = ( function ()
     createHeaders = function ( hostname )
     {
         var headers = igtPdLib.getCommonHeaders()
-        headers
+
         headers['x-ex-system-id'] = CA_SITE_CONSTANTS.SYSTEM_ID
         headers['x-site-id'] = CA_SITE_CONSTANTS.SITE_ID
         headers['x-channel-id'] = CA_PD_CONSTANTS.PWS_CHANNEL_ID
@@ -50,6 +50,7 @@ var IGTCas = ( function ()
         if ( hostname && hostname.match( /mobile/i ) )
         {
             headers['x-channel-id'] = CA_PD_CONSTANTS.MOBILE_CHANNEL_ID
+            headers['x-esa-api-key'] = 'AGpTKF0ze+6H6x3NTqjbVrMfcvt3MoZ7'
         }
         else
         {
@@ -59,7 +60,7 @@ var IGTCas = ( function ()
         return headers
     },
 
-    createLoginRequest = function ( hostname )
+    createLoginRequest = function ( hostname, username, password )
     {
         request = { siteId: CA_PD_CONSTANTS.SITE_ID }
 
@@ -70,6 +71,11 @@ var IGTCas = ( function ()
         else
         {
             request.clientId = CA_PD_CONSTANTS.PWS_CLIENT_ID
+        }
+
+        if (username && password )
+        {
+            request.resourceOwnerCredentials = { USERNAME: username, PASSWORD: password }
         }
 
         return request
